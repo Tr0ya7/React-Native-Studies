dcaeb1f439134dd89936cd66feedc6667538c769
Tr0ya7 <DevLuizHBarros@proton.me>
2025-06-16 14:47:34 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,37 +1,16 @@
-import { Picker } from "@react-native-picker/picker"
-import { useState } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import Slider from "@react-native-community/slider"
+import { useState } from "react"
 
 const App = () => {
-    const pizzas = [
-        {id: 1, name: 'Strogonoff', price: 35.9}, {id: 2, name: 'Pepperoni', price: 59}, {id: 3, name: 'Four chease', price: 37}, {id: 4, name: 'Brigadeiro', price: 25.7}, {id: 5, name: 'Portguesa', price: 30}
-    ]
-    const [selectedPizza, setSelectedPizza] = useState<null | number>(null)
-
-    let selectedPizzaIndex = selectedPizza! - 1
+    const [sliderValue, setSliderValue] = useState<number>(0)
 
     return (
         <View style={styles.container}>
-            <Text>
-                Menu Pizza:
+            <Text style={styles.sliderValueText}>
+                {sliderValue.toFixed()}
             </Text>
-            <Picker selectedValue={selectedPizza} onValueChange={setSelectedPizza}>
-                {pizzas.map((pizza) => <Picker.Item value={pizza.id} label={pizza.name} />)}
-            </Picker>
-            {selectedPizza !== null 
-                ? 
-                    <View style={styles.pizza}>
-                        <Text style={styles.logo}>
-                            You chose: {pizzas[selectedPizza - 1].name}
-                        </Text>
-                        <Text>
-                            {pizzas[selectedPizzaIndex].price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-                        </Text>
-                    </View>
-                :
-                    <></>
-            }
-            {selectedPizza !== null && <Text>{selectedPizzaIndex}</Text>}
+            <Slider minimumValue={0} maximumValue={100} minimumTrackTintColor="#00ff00" maximumTrackTintColor="#ff0000" value={sliderValue} onValueChange={setSliderValue} />
         </View>
     )
 }
@@ -39,19 +18,12 @@ const App = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 20
+        marginTop: 15
     },
 
-    logo: {
+    sliderValueText: {
         textAlign: 'center',
-        fontSize: 28,
-        fontWeight: 'bold'
-    },
-
-    pizza: {        
-        alignItems: 'center',
-        marginTop: 15,
-        fontSize: 25
+        fontSize: 30
     }
 })
 
